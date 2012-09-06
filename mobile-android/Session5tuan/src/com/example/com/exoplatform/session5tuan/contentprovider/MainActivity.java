36a8622919d9850a3c5b6b26950d8e4236e64f7a
Tuan Pham <tuanp@exoplatform.com>
2012-09-06 10:51:11 +0700

    MOB-1150 update query company name and job title

--- mobile-android/Session5tuan/src/com/example/com/exoplatform/session5tuan/contentprovider/MainActivity.java
+++ mobile-android/Session5tuan/src/com/example/com/exoplatform/session5tuan/contentprovider/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.com.exoplatform.session5tuan.contentprovider;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.ContactsContract.Data;
+import android.provider.ContactsContract.CommonDataKinds.Organization;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.app.Activity;
 import android.database.Cursor;
 import android.view.Menu;
@@ -26,13 +29,34 @@ public class MainActivity extends Activity {
         		
         Cursor cursor = getContacts();
          
+         
         while (cursor.moveToNext()) {
 
           String displayName = cursor.getString(cursor
               .getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
+          
+          String contactId = cursor.getString(cursor
+                  .getColumnIndex(ContactsContract.Contacts._ID));
+          
+          Cursor orgCur = getCompanyOf(contactId) ;
+          String orgName = "" ;
+          String title = "";
+          while(orgCur.moveToNext()) {
+          orgName = orgCur.getString(orgCur.getColumnIndex(Organization.COMPANY)); 
+  		  title = orgCur.getString(orgCur.getColumnIndex(Organization.TITLE));
+          }
+          orgCur.close() ;
+          
+          
           contactView.append("Name: ");
           contactView.append(displayName);
           contactView.append("\n");
+          contactView.append(orgName) ;
+          contactView.append("\n");
+          contactView.append(title) ;
+          contactView.append("\n");
+          
+          
         }
         
         layout_.addView(contactView) ;
@@ -43,7 +67,7 @@ public class MainActivity extends Activity {
         // Run query
         Uri uri = ContactsContract.Contacts.CONTENT_URI;
         String[] projection = new String[] { ContactsContract.Contacts._ID,
-            ContactsContract.Contacts.DISPLAY_NAME };
+            ContactsContract.Contacts.DISPLAY_NAME};
         String selection = ContactsContract.Contacts.IN_VISIBLE_GROUP + " = '"
             + ("1") + "'";
         String[] selectionArgs = null;
@@ -54,6 +78,17 @@ public class MainActivity extends Activity {
         return managedQuery(uri, projection, selection, selectionArgs,
             sortOrder);
       }
+    
+    private Cursor getCompanyOf(String contactId) {
+    	
+    	return getContentResolver().query(
+    			Data.CONTENT_URI,                         // URI
+    	        new String[] {Organization.COMPANY, Organization.TITLE},
+    	        Data.CONTACT_ID + "=?",                   // Selection
+    	        new String[] {String.valueOf(contactId)}, // Selection arguments
+    	        null);
+    	
+    }
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
