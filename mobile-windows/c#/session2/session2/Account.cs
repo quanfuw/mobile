6f971bd39c284c57dc8b490f13486810a7987cd3
Tuan Pham <phamtuanchip@gmail.com>
2012-09-26 00:31:43 +0700

    session2 thread

---
+++ mobile-windows/c#/session2/session2/Account.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session2
+{
+    class Account
+    {
+        private Object thisLock = new Object();
+        int balance;
+
+        Random r = new Random();
+
+        public Account(int initial)
+        {
+            balance = initial;
+        }
+
+        int Withdraw(int amount)
+        {
+
+            // This condition will never be true unless the lock statement
+            // is commented out:
+            if (balance < 0)
+            {
+                throw new Exception("Negative Balance");
+            }
+
+            // Comment out the next line to see the effect of leaving out 
+            // the lock keyword:
+            lock (thisLock)
+            {
+                if (balance >= amount)
+                {
+                    Console.WriteLine("Balance before Withdrawal :  " + balance);
+                    Console.WriteLine("Amount to Withdraw        : -" + amount);
+                    balance = balance - amount;
+                    Console.WriteLine("Balance after Withdrawal  :  " + balance);
+                    return amount;
+                }
+                else
+                {
+                    return 0; // transaction rejected
+                }
+            }
+        }
+
+        public void DoTransactions()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Withdraw(r.Next(1, 100));
+            }
+        }
+    }
+}
---
+++ mobile-windows/c#/session2/session2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace session2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[10];
+            Account acc = new Account(1000);
+            for (int i = 0; i < 10; i++)
+            {
+                Thread t = new Thread(new ThreadStart(acc.DoTransactions));
+                threads[i] = t;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i].Start();
+            }
+            Console.Read();
+        }
+    }
+}
