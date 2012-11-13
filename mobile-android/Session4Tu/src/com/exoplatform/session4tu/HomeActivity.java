f426b08e4f097d1c2421b98f39be1c67e2e2f661
anhtu <anhtu2687@gmail.com>
2012-11-13 18:08:55 +0700

    [MOB-1369] Session 4 initial code

---
+++ mobile-android/Session4Tu/src/com/exoplatform/session4tu/HomeActivity.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2003-2012 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.exoplatform.session4tu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * @author Created by The eXo Platform SAS
+ * <br/>Anh-Tu Nguyen
+ * <br/><a href="mailto:tuna@exoplatform.com">tuna@exoplatform.com<a/>
+ * Nov 13, 2012  
+ */
+public class HomeActivity extends Activity
+{
+  private static final String TAG = "HomeActivity";
+  
+  @Override
+  public void onCreate(Bundle bundle)
+  {
+    super.onCreate(bundle);
+    Log.i(TAG, "onCreate");
+    
+    Intent downloadIntent = new Intent();
+    downloadIntent.setAction("com.exoplatform.intent.Download");
+    startService(downloadIntent);
+  }
+  
+  
+}
