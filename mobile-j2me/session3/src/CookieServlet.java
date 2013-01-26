16fedc22c3ed6ae5fdf4cc48eb95195d5611244e
Pham Tuan <tuanp@exoplatform.com>
2013-01-26 23:45:36 +0700

    connections

---
+++ mobile-j2me/session3/src/CookieServlet.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+
+public class CookieServlet extends HttpServlet {
+  private Map mHitMap = new HashMap();
+
+  public void doGet(HttpServletRequest request,
+      HttpServletResponse response)
+      throws ServletException, IOException {
+    HttpSession session = request.getSession();
+
+    String id = session.getId();
+
+    int hits = -1;
+
+    // Try to retrieve the hits from the map.
+    Integer hitsInteger = (Integer)mHitMap.get(id);
+    if (hitsInteger != null)
+      hits = hitsInteger.intValue();
+
+    // Increment and store.
+    hits++;
+    mHitMap.put(id, new Integer(hits));
+    String message = "Hits for this session: " + hits + ".";
+
+    response.setContentType("text/plain");
+    response.setContentLength(message.length());
+    PrintWriter out = response.getWriter();
+    out.println(message);
+  }
+}
