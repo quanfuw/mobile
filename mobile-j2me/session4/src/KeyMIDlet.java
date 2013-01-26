b702a9a2f9a16b6fe5a6202ca9a374648a986f02
Pham Tuan <tuanp@exoplatform.com>
2013-01-26 23:10:42 +0700

    Canvas implement

---
+++ mobile-j2me/session4/src/KeyMIDlet.java
@@ -0,0 +1,25 @@
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.midlet.MIDlet;
+
+public class KeyMIDlet
+    extends MIDlet{
+  public void startApp() {
+    Displayable d = new KeyCanvas();
+
+    d.addCommand(new Command("Exit", Command.EXIT, 0));
+    d.setCommandListener(new CommandListener() {
+      public void commandAction(Command c, Displayable s) {
+        notifyDestroyed();
+      }
+    } );
+
+    Display.getDisplay(this).setCurrent(d);
+  }
+
+  public void pauseApp() { }
+
+  public void destroyApp(boolean unconditional) { }
+}
